d4dd05d80fec5d1694c11eee0f859498685f70b1
Damis Garcia <damisgarcia@gmail.com>
2018-05-25 22:43:29 -0300

    Finish Prototype

--- src/components/LandPageContent.vue
+++ src/components/LandPageContent.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class='container'>
-    <p>Cillum eiusmod occaecat dolore occaecat magna est laborum.</p>
-  </div>
+  <section>
+    <h2>Lorem ipsum dolor sit</h2>
+    <p>Reprehenderit dolore laboris incididunt occaecat consectetur tempor. Reprehenderit esse enim ipsum id enim nisi irure esse mollit fugiat ullamco culpa ex. Veniam quis laborum exercitation excepteur pariatur quis amet fugiat ullamco deserunt esse. Non nisi labore deserunt eiusmod veniam excepteur mollit occaecat commodo. Ullamco Lorem culpa sunt sint.</p>
+    <p>Incididunt adipisicing exercitation deserunt aliquip proident et dolore. Eu sit incididunt nulla exercitation proident laborum esse. Minim adipisicing aliquip sint id do sint officia aliquip ipsum deserunt elit. Aliqua aliqua aute veniam aute ea Lorem voluptate. Adipisicing enim duis proident in voluptate. Non Lorem aliqua commodo veniam culpa incididunt duis reprehenderit sunt dolor anim exercitation sunt. Officia nulla dolore proident aliqua est.</p>
+    <p>Ipsum eu laboris pariatur ut ex consectetur excepteur eu. Occaecat ullamco commodo nulla non aliquip occaecat consequat enim anim exercitation duis anim. Dolor proident amet excepteur ex ut fugiat laborum labore enim. Excepteur nulla ipsum ipsum consequat amet aliqua reprehenderit nulla officia dolore excepteur. In veniam tempor fugiat labore mollit amet irure. Adipisicing laboris commodo irure ea ex.</p>
+    <p>Lorem culpa nostrud labore do in ad. Amet velit pariatur dolore eu sunt consequat eiusmod amet nostrud dolor dolor. Voluptate reprehenderit ipsum pariatur officia est amet qui. Anim sint irure officia cupidatat cupidatat anim. Labore duis voluptate enim nulla non ipsum sunt minim amet mollit dolor.</p>
+    <p>Pollit minim do dolore minim in. Cupidatat minim adipisicing laborum ullamco. Ipsum enim velit in ut officia ea quis veniam. Officia veniam voluptate est Lorem amet veniam. Nisi veniam et sit esse reprehenderit tempor et exercitation officia amet tempor adipisicing nulla. Nostrud aute ut ullamco aliqua anim mollit laboris nulla voluptate. Occaecat incididunt ullamco Lorem enim proident fugiat nostrud deserunt laboris cupidatat.</p>
+  </section>
 </template>
 
 <script>
--- src/components/LandPageHeader.vue
+++ src/components/LandPageHeader.vue
@@ -1,6 +1,11 @@
 <template>
   <header class="landpage-header" v-bind:style="{ 'background-image':'url( '+ bg + ')' }">
-    <h3>A maneira mais simples de gerênciar o seu condomínio</h3>
+    <img class="logo" width="128px" src='@/assets/white.svg' />
+    <div class="title">A maneira mais simples de gerênciar <br> o seu condomínio</div>
+    <div class="devices">
+      <img src='@/assets/devices.png' />
+      <small>Disponível na Apple Store e Playstore.</small>
+    </div>
   </header>
 </template>
 <script>
