cdde8c53bd4aabc16917789b6d3cc6a0230935bb
Damis Garcia <damisgarcia@gmail.com>
2018-05-25 23:17:37 -0300

    Update build

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/docondominio-homepage',
+    assetsPublicPath: '/docondominio-homepage/',
 
     /**
      * Source Maps
